9196ab3e7851cfc869bcb9b677d9a08878a4b835
Tyler Hancock <darklime@live.ca>
2017-05-25 00:23:38 -0600

    Updated to 1.11.2

--- src/main/java/net/darkhax/enchdesc/EnchantmentDescriptions.java
+++ src/main/java/net/darkhax/enchdesc/EnchantmentDescriptions.java
@@ -13,31 +13,34 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
-@Mod(modid = Constants.MODID, name = Constants.MOD_NAME, version = Constants.VERSION_NUMBER, acceptedMinecraftVersions = "[1.9.4,1.10.2]")
+@Mod(modid = Constants.MODID, name = Constants.MOD_NAME, version = "@VERSION@")
 public class EnchantmentDescriptions {
-    
+
     @Mod.Instance(Constants.MODID)
     public static EnchantmentDescriptions instance;
-    
+
     @EventHandler
     public void preInit (FMLPreInitializationEvent event) {
-        
-        if (FMLCommonHandler.instance().getMinecraftServerInstance() != null && FMLCommonHandler.instance().getMinecraftServerInstance().isDedicatedServer())
+
+        if (FMLCommonHandler.instance().getMinecraftServerInstance() != null && FMLCommonHandler.instance().getMinecraftServerInstance().isDedicatedServer()) {
             Constants.LOG.warn("This is a client side mod. There is no benefit to installing it server side.");
-            
+        }
         else if (event.getSide().equals(Side.CLIENT)) {
-            
+
             ConfigurationHandler.initConfig(event.getSuggestedConfigurationFile());
             MinecraftForge.EVENT_BUS.register(new TooltipHandler());
         }
     }
-    
+
     @EventHandler
     public void postInit (FMLPostInitializationEvent event) {
-        
-        if (event.getSide().equals(Side.CLIENT) && ConfigurationHandler.exploreMode)
-            for (Enchantment enchant : Enchantment.REGISTRY)
-                if (I18n.format(TooltipHandler.getTranslationKey(enchant)).startsWith("enchantment."))
+
+        if (event.getSide().equals(Side.CLIENT) && ConfigurationHandler.exploreMode) {
+            for (final Enchantment enchant : Enchantment.REGISTRY) {
+                if (I18n.format(TooltipHandler.getTranslationKey(enchant)).startsWith("enchantment.")) {
                     Constants.LOG.info(String.format("Undefined enchantment from %s %s", TooltipHandler.getModName(enchant), TooltipHandler.getTranslationKey(enchant)));
+                }
+            }
+        }
     }
 }
--- src/main/java/net/darkhax/enchdesc/client/TooltipHandler.java
+++ src/main/java/net/darkhax/enchdesc/client/TooltipHandler.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import com.mojang.realmsclient.gui.ChatFormatting;
 
-import net.darkhax.enchdesc.lib.Constants;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.settings.GameSettings;
@@ -24,107 +23,107 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
 public class TooltipHandler {
-    
+
     private static final KeyBinding keyBindSneak = Minecraft.getMinecraft().gameSettings.keyBindSneak;
-    
+
     @SubscribeEvent
     public void onTooltipDisplayed (ItemTooltipEvent event) {
-        
+
         if (event.getItemStack() != null && event.getItemStack().getItem() instanceof ItemEnchantedBook) {
-            
+
             final List<String> tooltip = event.getToolTip();
-            
+
             if (GameSettings.isKeyDown(keyBindSneak)) {
-                
+
                 final ItemEnchantedBook item = (ItemEnchantedBook) event.getItemStack().getItem();
-                final List<Enchantment> enchants = getEnchantments(item, event.getItemStack());
-                
-                for (Enchantment enchant : enchants) {
-                    
+                final List<Enchantment> enchants = this.getEnchantments(item, event.getItemStack());
+
+                for (final Enchantment enchant : enchants) {
+
                     tooltip.add(I18n.format("tooltip.enchdesc.name") + ": " + I18n.format(enchant.getName()));
-                    tooltip.add(getDescription(enchant));
+                    tooltip.add(this.getDescription(enchant));
                     tooltip.add(I18n.format("tooltip.enchdesc.addedby") + ": " + ChatFormatting.BLUE + getModName(enchant));
                 }
             }
-            
-            else
+            else {
                 tooltip.add(I18n.format("tooltip.enchdesc.activate", ChatFormatting.LIGHT_PURPLE, keyBindSneak.getDisplayName(), ChatFormatting.GRAY));
+            }
         }
     }
-    
+
     /**
      * Gets the description of the enchantment. Or the missing text, if no description exists.
-     * 
+     *
      * @param enchantment The enchantment to get a description for.
      * @return The enchantment description.
      */
     private String getDescription (Enchantment enchantment) {
-        
+
         final String key = getTranslationKey(enchantment);
         String description = I18n.format(key);
-        
-        if (description.startsWith("enchantment."))
+
+        if (description.startsWith("enchantment.")) {
             description = I18n.format("tooltip.enchdesc.missing", getModName(enchantment), key);
-            
+        }
+
         return description;
     }
-    
+
     /**
      * Reads a List of enchantments from an ItemEnchantedBook stack.
-     * 
+     *
      * @param book Instance of ItemEnchantedBook, as it uses non-static methods for some
      *        reason.
      * @param stack The stack to read the data from.
      * @return The list of enchantments stored on the stack.
      */
     private List<Enchantment> getEnchantments (ItemEnchantedBook book, ItemStack stack) {
-        
+
         final NBTTagList enchTags = book.getEnchantments(stack);
-        final List<Enchantment> enchantments = new ArrayList<Enchantment>();
-        
+        final List<Enchantment> enchantments = new ArrayList<>();
+
         if (enchTags != null) {
-            
+
             for (int index = 0; index < enchTags.tagCount(); ++index) {
-                
+
                 final int id = enchTags.getCompoundTagAt(index).getShort("id");
                 final Enchantment enchant = Enchantment.getEnchantmentByID(id);
-                
-                if (enchant != null)
+
+                if (enchant != null) {
                     enchantments.add(enchant);
+                }
             }
         }
-        
+
         return enchantments;
     }
-    
+
     /**
      * Gets the name of the mod that registered the passed object. Works for anthing which uses
      * Forge's registry.
-     * 
+     *
      * @param registerable The object to get the mod name of.
      * @return The name of the mod which registered the object.
      */
     public static String getModName (IForgeRegistryEntry.Impl<?> registerable) {
-        
-    	if (registerable != null && registerable.getRegistryName() != null) {
-    		
+
+        if (registerable != null && registerable.getRegistryName() != null) {
+
             final String modID = registerable.getRegistryName().getResourceDomain();
             final ModContainer mod = Loader.instance().getIndexedModList().get(modID);
             return mod != null ? mod.getName() : modID;
-    	}
-    	
-    	return "NULL";
-    }
-    
-    public static String getTranslationKey(Enchantment enchant) {
-        
-    	if (enchant != null) {
+        }
 
-    		Constants.LOG.info(enchant.getClass());
-    		
-    		if (enchant.getRegistryName() != null)
-    			return String.format("enchantment.%s.%s.desc", enchant.getRegistryName().getResourceDomain(), enchant.getRegistryName().getResourcePath());
-    	}
-    	return "enchantment. NULL";
+        return "NULL";
+    }
+
+    public static String getTranslationKey (Enchantment enchant) {
+
+        if (enchant != null && enchant.getRegistryName() != null) {
+
+            return String.format("enchantment.%s.%s.desc", enchant.getRegistryName().getResourceDomain(), enchant.getRegistryName().getResourcePath());
+        }
+
+        return "NULL";
     }
 }
