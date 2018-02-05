3ab0803868b9deb47c80efcdd467120b68d6d36a
Tyler Hancock <darklime@live.ca>
2018-02-05 00:37:43 -0700

    Cleaned up client side code. Mod will be ignored on servers.

--- src/main/java/net/darkhax/enchdesc/EnchantmentDescriptions.java
+++ src/main/java/net/darkhax/enchdesc/EnchantmentDescriptions.java
@@ -8,37 +8,36 @@ import net.darkhax.enchdesc.handler.ConfigurationHandler;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-@Mod(modid = "enchdesc", name = "Enchantment Descriptions", version = "@VERSION@", certificateFingerprint = "@FINGERPRINT@")
+@Mod(modid = "enchdesc", name = "Enchantment Descriptions", version = "@VERSION@", clientSideOnly = true, certificateFingerprint = "@FINGERPRINT@")
 public class EnchantmentDescriptions {
 
     public static final Logger LOG = LogManager.getLogger("Enchantment Descriptions");
 
     @EventHandler
+    @SideOnly(Side.CLIENT)
     public void preInit (FMLPreInitializationEvent event) {
 
-        if (FMLCommonHandler.instance().getMinecraftServerInstance() != null && FMLCommonHandler.instance().getMinecraftServerInstance().isDedicatedServer()) {
-            LOG.warn("This is a client side mod. There is no benefit to installing it server side.");
-        }
-        else if (event.getSide().equals(Side.CLIENT)) {
-
-            ConfigurationHandler.initConfig(event.getSuggestedConfigurationFile());
-            MinecraftForge.EVENT_BUS.register(new TooltipHandler());
-        }
+        ConfigurationHandler.initConfig(event.getSuggestedConfigurationFile());
+        MinecraftForge.EVENT_BUS.register(new TooltipHandler());
     }
 
     @EventHandler
+    @SideOnly(Side.CLIENT)
     public void postInit (FMLPostInitializationEvent event) {
 
-        if (event.getSide().equals(Side.CLIENT) && ConfigurationHandler.isExploreMode()) {
+        if (ConfigurationHandler.isExploreMode()) {
+
             for (final Enchantment enchant : Enchantment.REGISTRY) {
+
                 if (I18n.format(TooltipHandler.getTranslationKey(enchant)).startsWith("enchantment.")) {
+
                     LOG.info(String.format("Undefined enchantment from %s %s", TooltipHandler.getModName(enchant), TooltipHandler.getTranslationKey(enchant)));
                 }
             }
