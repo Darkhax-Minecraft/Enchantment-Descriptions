9942f18f551176599f6679fdf008a89025af8682
Tyler Hancock <darklime@live.ca>
2018-02-05 00:31:16 -0700

    Removed constants class.

--- src/main/java/net/darkhax/enchdesc/EnchantmentDescriptions.java
+++ src/main/java/net/darkhax/enchdesc/EnchantmentDescriptions.java
@@ -1,8 +1,10 @@
 package net.darkhax.enchdesc;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import net.darkhax.enchdesc.client.TooltipHandler;
 import net.darkhax.enchdesc.handler.ConfigurationHandler;
-import net.darkhax.enchdesc.lib.Constants;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraftforge.common.MinecraftForge;
@@ -13,14 +15,16 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
-@Mod(modid = Constants.MODID, name = Constants.MOD_NAME, version = "@VERSION@", certificateFingerprint = "@FINGERPRINT@")
+@Mod(modid = "enchdesc", name = "Enchantment Descriptions", version = "@VERSION@", certificateFingerprint = "@FINGERPRINT@")
 public class EnchantmentDescriptions {
-
+	
+    public static final Logger LOG = LogManager.getLogger("Enchantment Descriptions");
+    
     @EventHandler
     public void preInit (FMLPreInitializationEvent event) {
 
         if (FMLCommonHandler.instance().getMinecraftServerInstance() != null && FMLCommonHandler.instance().getMinecraftServerInstance().isDedicatedServer()) {
-            Constants.LOG.warn("This is a client side mod. There is no benefit to installing it server side.");
+            LOG.warn("This is a client side mod. There is no benefit to installing it server side.");
         }
         else if (event.getSide().equals(Side.CLIENT)) {
 
@@ -35,7 +39,7 @@ public class EnchantmentDescriptions {
         if (event.getSide().equals(Side.CLIENT) && ConfigurationHandler.isExploreMode()) {
             for (final Enchantment enchant : Enchantment.REGISTRY) {
                 if (I18n.format(TooltipHandler.getTranslationKey(enchant)).startsWith("enchantment.")) {
-                    Constants.LOG.info(String.format("Undefined enchantment from %s %s", TooltipHandler.getModName(enchant), TooltipHandler.getTranslationKey(enchant)));
+                    LOG.info(String.format("Undefined enchantment from %s %s", TooltipHandler.getModName(enchant), TooltipHandler.getTranslationKey(enchant)));
                 }
             }
         }
