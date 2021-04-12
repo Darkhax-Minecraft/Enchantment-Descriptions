9420ae00140a6c34e041f4cac2363d507746e656
Tyler Hancock <darklime@live.ca>
2021-04-11 23:31:23 -0600

    Fix Sponge maven location.

--- src/main/java/net/darkhax/enchdesc/jei/EnchantmentDescriptionsJEIPlugin.java
+++ src/main/java/net/darkhax/enchdesc/jei/EnchantmentDescriptionsJEIPlugin.java
@@ -1,5 +1,6 @@
 package net.darkhax.enchdesc.jei;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,6 +10,7 @@ import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
+import mezz.jei.api.runtime.IJeiRuntime;
 import net.darkhax.enchdesc.EnchantmentDescriptions;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
@@ -45,5 +47,16 @@ public class EnchantmentDescriptionsJEIPlugin implements IModPlugin {
     public void registerCategories (IRecipeCategoryRegistration registration) {
         
         registration.addRecipeCategories(new CategoryEnchantmentInfo(registration.getJeiHelpers().getGuiHelper()));
+    }
+    
+    @Override
+    public void onRuntimeAvailable (IJeiRuntime jeiRuntime) {
+        
+        final List<ItemStack> items = new ArrayList<>();
+        items.add(new ItemStack(Items.GOLDEN_HELMET));
+        items.add(new ItemStack(Items.GOLDEN_LEGGINGS));
+        items.add(new ItemStack(Items.TURTLE_HELMET));
+        
+        jeiRuntime.getIngredientManager().removeIngredientsAtRuntime(VanillaTypes.ITEM, items);
     }
 }
