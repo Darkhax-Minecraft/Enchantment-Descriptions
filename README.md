4368631b2fbdd3c45a3514d8984009faf3d2494e
Ilesh <ileshkt@gmail.com>
2021-11-22 21:04:08 +0530

    Code improvements. (almost) Full changelog: - Enchantment descriptions are only shown when holding shift - `enchdesc.tooltip.activate` is shown when not holding shift - Replaced iterators and while loops with for loops - Fixed mod_id in `fabric.mod.json` - Improved readme - Updated dependencies in the `gradle.properties` - Improved `build.gradle` - Updated Gradle wrapper to support Java 17 - Edited all the language files to be consistent with `en_us.json`, previously the lang files were _wildy_ inconsistent with each other - I have also added comments to lang files which have missing enchantments
    Signed-off-by: Ilesh <ileshkt@gmail.com>

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # Enchantment-Descriptions [![](http://cf.way2muchnoise.eu/250419.svg)](https://minecraft.curseforge.com/projects/enchantment-descriptions) [![](http://cf.way2muchnoise.eu/versions/250419.svg)](https://minecraft.curseforge.com/projects/enchantment-descriptions)
+
 Displays descriptions about an enchantment on the tooltip.
+Works best with [Tooltip Fix](https://github.com/kyrptonaught/tooltipfix).
 
 [![Nodecraft](https://nodecraft.com/assets/images/logo-dark.png)](https://nodecraft.com/r/darkhax)
-This project is sponsored by Nodecraft. Use code [Darkhax](https://nodecraft.com/r/darkhax) for 30% off your first month of service!
+This project is sponsored by Nodecraft. Use code [Darkhax](https://nodecraft.com/r/darkhax) for 30% off your first month
+of service!
 
-## Adding support
+## Adding support for more languages
 
-This mod uses a simple format for enchantment descriptions. You can add support for this mod and others using the format by adding a translation key to your language file. The format is `enchantment.MODID.ENDID.desc=`.
+This mod uses a simple format for translating enchantment descriptions. You can add languages to this mod using by
+adding a translation key to your language file. The translation key format used
+is `enchantment.MODID.ENCHANTMENT_ID.desc`. For example, the Minecraft thorns enchantment has the translation
+key: `enchantment.minecraft.thorns.desc`
+
--- src/main/java/net/darkhax/enchdesc/EnchantmentDescriptions.java
+++ src/main/java/net/darkhax/enchdesc/EnchantmentDescriptions.java
@@ -1,13 +1,10 @@
 package net.darkhax.enchdesc;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
-
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
@@ -17,42 +14,50 @@ import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
 
+import java.util.List;
+import java.util.Set;
+
 @Environment(EnvType.CLIENT)
 public class EnchantmentDescriptions implements ClientModInitializer {
-	
-	@Override
-	public void onInitializeClient() {
-		
-		ItemTooltipCallback.EVENT.register(EnchantmentDescriptions::onTooltipDisplayed);
-	}
-	
-    private static void onTooltipDisplayed (ItemStack stack, TooltipContext context, List<Text> lines) {
-        
-    	if (stack.getItem() instanceof EnchantedBookItem && !EnchantmentHelper.get(stack).isEmpty()) {
-    		
-            insertDescriptionTooltips(lines, stack);
-    	}
+  @Override
+  public void onInitializeClient() {
+    ItemTooltipCallback.EVENT.register(EnchantmentDescriptions::onTooltipDisplayed);
+  }
+
+  private static void onTooltipDisplayed(ItemStack stack, TooltipContext context, List<Text> lines) {
+    Set<Enchantment> enchants = EnchantmentHelper.get(stack).keySet();
+    if (
+            stack.getItem() instanceof EnchantedBookItem &&
+                    !enchants.isEmpty()
+    ) {
+      if (Screen.hasShiftDown()) {
+        insertDescriptionTooltips(lines, enchants);
+      } else {
+        lines.add(
+                new TranslatableText(
+                        "enchdesc.tooltip.activate",
+                        "shift"
+                ).formatted(Formatting.DARK_GRAY)
+        );
+      }
     }
-    
-    private static void insertDescriptionTooltips (List<Text> tips, ItemStack stack) {
-        
-        final Iterator<Enchantment> enchants = EnchantmentHelper.get(stack).keySet().iterator();
-        
-        while (enchants.hasNext()) {
-            
-            final Enchantment enchant = enchants.next();
-            final ListIterator<Text> tooltips = tips.listIterator();
-            
-            while (tooltips.hasNext()) {
-                
-                final Text tooltipLine = tooltips.next();
-                
-                if (tooltipLine instanceof TranslatableText && ((TranslatableText) tooltipLine).getKey().equals(enchant.getTranslationKey())) {
-                    
-                    tooltips.add(new TranslatableText(enchant.getTranslationKey() + ".desc").formatted(Formatting.DARK_GRAY));
-                    break;
-                }
-            }
+  }
+
+  private static void insertDescriptionTooltips(List<Text> lines, Set<Enchantment> enchants) {
+    for (Enchantment enchant : enchants) {
+      for (Text tooltipLine : lines) {
+        if (
+                tooltipLine instanceof TranslatableText &&
+                        ((TranslatableText) tooltipLine).getKey().equals(enchant.getTranslationKey())
+        ) {
+          lines.add(
+                  lines.indexOf(tooltipLine) + 1,
+                  new TranslatableText(enchant.getTranslationKey() + ".desc")
+                          .formatted(Formatting.DARK_GRAY)
+          );
+          break;
         }
+      }
     }
+  }
 }
